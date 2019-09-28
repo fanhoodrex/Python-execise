bb5b6b2eb612c2a66e0cafcc32fa53da98d9bcdf
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-09-28 23:10:11 +0800

    Update Sololearn.py

--- Sololearn.py
+++ Sololearn.py
@@ -80,4 +80,200 @@ tab = [1,2,3,4,5,6]
 dic = {1:2,3:4,5:6}
 print(tup[1] + tab[2] + dic[3])
 
-#14
+#14 combination
+count = 0
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if i != j and i != k and j != k:
+                print(i+j*10+k*100)
+                count += 1
+print("一共有%d个三位数" % count)
+
+#15 水仙花数
+for number in range(100, 1000):
+    i = number % 10
+    j = number // 10 % 10
+    k = number // 100
+    if number == i**3+j**3+k**3:
+        print(number)
+
+#16 猜字游戏
+true_number = int(input("请主持人输入数字："))
+low_number = int(input("请主持人输入范围下限："))
+high_number = int(input("请主持人输入范围上限："))
+i = 1
+while i <= 5:
+    guess_number = int(input("数字的范围是%d-%d，现在第%d次猜测：" % (low_number, high_number, i)))
+    if true_number == guess_number:
+        print("恭喜你猜对啦！")
+        break
+    elif guess_number < true_number and guess_number > low_number:
+        low_number = guess_number
+    elif guess_number > true_number and guess_number < high_number:
+        high_number = guess_number
+    i = i + 1
+    if i > 5:
+        print("很遗憾，你五次都没有猜对。正确数字是%d。" % true_number)
+
+#17 九九乘法表
+for i in range(1, 10):
+    for j in range(i, 10):
+        print("%dx%d=%d" % (i, j, i*j), end="\t")
+    print('\n')
+
+#18 ???
+x = "abcda"
+for i in x:
+    if x.index(i) == 0:print(i)
+
+#19 变量内存地址的指针不同
+I = [1,9,65,88,4,0]
+a = I.sort()
+b = sorted(I)
+if a == b:
+    print(1+1)
+else:
+    print(0)
+
+#20 ???
+print('abef'.partition('cd'))
+
+#21 no output? 
+a = 4
+b = 2
+c = 0
+if (a<b) < c:
+    print(0)
+a,c = c,a
+if (a>b)>c:
+    print(1)
+
+#22 id of a variable 内存地址一样
+x,y =0,True
+x = x + 1
+y = int(True) # convert bool to value
+print(id(x)==id(y))
+
+#23 list + list
+a = [1,2,3]
+def add(n):
+    a += [n]
+add(4)
+print(a)
+
+#24
+a,b,*c = [1,2,3,7]
+print(*c) # 3,7
+print(c) # list type
+
+#25 ???
+print([12,44],[123,'sri',(1,2,3)],'Hello'][2][2][2])
+
+#26 for
+b = 3
+a = 1
+for i in range(b):
+    a = a + a*i
+print(a)
+
+i= 0,1,2
+
+a = 1
+a = 2 
+a = 6
+
+#27 ??
+def numbers(x):
+    for i in range(x):
+        if i % 4 ==3:
+            yield i
+print(list(number(11)))
+
+#28
+o = "j"
+e = "a"
+o = o + e +"v"
+o = o + e
+m = o
+print(m)
+o = o + 's'
+print(o)
+print(m) #
+
+# 29 知识点和28题一样
+a = b = 1
+b = 2
+print(a,b)
+
+#30 anything is value, except None 
+def is_it_true(anything):
+    if anything:
+        print("True")
+    else:
+        print("False")
+is_it_true(None)
+is_it_true(1)
+is_it_true('str')
+
+#31 ???
+def run(a):
+    return 2 * a
+print(run(run(a)))
+
+#32 math 和 阶乘
+import math
+print(math.factorial(4.0))
+
+#33 try except handler
+import math
+try:
+    print(math.sqrt(81,'s'),end="")
+except:
+    print(0,end="")
+finally:
+    print(x,end="")
+
+#34
+a = [8,0,4,6,1]
+if (a is a[:]):
+    print(True)
+else:
+    print(False)
+print(a[:])
+
+#35
+num  = 5
+sum = 0
+while True:
+    if num >= 1:
+        sum += num
+        num -= 1
+    else:
+        break
+print(sum)
+
+num = 5,4,3,2,1
+sum = 5,9,12,14,15
+
+#36
+list = [1,2,3,4,6] 
+if len(list) % 2 != 0: # len(list) = 5 
+    print(list[max(list)%4])
+else:
+    print(list[min(list)])
+
+max(list) = 6 
+max(list)%4 = 2
+list[max(list)%4] =3
+
+#37 reverse 
+a = [1,2,3,4,5]
+print(a[-1:None:-2])
+print(a[-1::-2])
+
+#38 ??? 
+print(r"\nSololearn")
+
+#39
+
