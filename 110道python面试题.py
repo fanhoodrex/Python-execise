7cd336416e1a836bededef064dd0014ff98b7500
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-11-06 16:59:52 +0800

    Update 110道python面试题.py
    progress now at 40

--- 110道python面试题.py
+++ 110道python面试题.py
@@ -41,6 +41,7 @@ print(dic_merge)
 #6、python实现列表去重的方法
 list = [11,12,13,12,15,16,13]
 a = set(list)
+a = list(a)
 print(a)
 print([x for x in a])
 
@@ -132,11 +133,154 @@ print("assertation wrong,program error")
 """ls  pwd  cd  touch  rm  mkdir  tree  
 cp  mv  cat  more  grep  echo"""
 
+#20、python2和python3区别？列举5个
+"""
+1、Python3 使用 print 必须要以小括号包裹打印内容，比如 print('hi')
+Python2 既可以使用带小括号的方式，也可以使用一个空格来分隔打印内容，比如 print 'hi'
+2、python2 range(1,10)返回列表，python3中返回迭代器，节约内存
+3、python2中使用ascii编码，python中使用utf-8编码
+4、python2中unicode表示字符串序列，str表示字节序列
+python3中str表示字符串序列，byte表示字节序列
+5、python2中为正常显示中文，引入coding声明，python3中不需要
+6、python2中是raw_input()函数，python3中是input()函数
+"""
 
+#21、列出python中可变数据类型和不可变数据类型，并简述原理
+a,b = 3,3
+print(id(a))
+print(id(b))
 
+#22、s = "ajldjlajfdljfddd"，去重并从小到大排序输出"adfjl"
+s = "ajldjlajfdljfddd"
+s = set(s)
+s = list(s)
+s.sort(reverse=False)
+res = "".join(s)
+print(res)
 
+#23、用lambda函数实现两个数相乘
+sum = lambda a,b:a*b
+print(sum(5,4))
 
+#24、字典根据键从小到大排序
+dic={"name":"zs","age":18,"city":"深圳","tel":"1362626627"}
+lis = sorted(dic.items(),key=lambda i:i[0],reverse= False)
+print(lis)
+print(dict(lis))
 
+#25、利用collections库的Counter方法统计字符串每个单词出现的次数
+#"kjalfj;ldsjafl;hdsllfdhg;lahfbl;hl;ahlf;h"
+from collections import Counter
+a = "kjalfj;ldsjafl;hdsllfdhg;lahfbl;hl;ahlf;h"
+res = Counter(a)
+print(res)
+
+#26、字符串a = "not 404 found 张三 99 深圳"，每个词中间是空格，
+#用正则过滤掉英文和数字，最终输出"张三  深圳"
+import re
+a = "not 404 found 张三 99 深圳"
+list = a.split(" ")
+print(list)
+res = re.findall('\d+[a-zA-Z]+',a)
+for i in res:
+    if i in list:
+        list.remove(i)
+new_str = " ".join(list)
+print(res,"\n",new_str)
+
+#27、filter方法求出列表所有奇数并构造新列表，
+#a =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = [x for x in range(1,11)]
+def fn(a):
+    if a%2 == 1:
+        return a
+newlist = filter(fn,a)
+newlist = [i for i in newlist]
+print(newlist)
+
+#28、列表推导式求列表所有奇数并构造新列表，
+#a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = [x for x in range(1,11)]
+res = [i for i in a if i%2==1]
+print(res)
+
+#29、正则re.complie作用
+#re.compile是将正则表达式编译成一个对象，加快速度，并重复使用
+
+#30、a=（1，）b=(1)，c=("1") 分别是什么类型的数据？
+a=type((1))
+b=type(("1"))
+c=type((1,))
+print(a,b,c)
+
+#31、两个列表[1,5,7,9]和[2,2,6,8]合并为[1,2,2,3,6,7,8,9]
+list1 = [i for i in range(1,10,2)]
+list2 = [2,2,6,8]
+list1.extend(list2)
+print(list1)
+list1.sort(reverse= False)
+print(list1)
+
+#32、用python删除文件和用linux命令删除文件方法
+"""
+python： os.remove(文件名)
+linux: rm  文件名
+"""
+
+#33、log日志中，我们需要用时间戳记录error,warning
+#等的发生时间，请用datetime模块打印当前时间戳
+#“2018-04-01 11:38:54”
+import datetime
+a = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+'  星期:'+str(datetime.datetime.now().isoweekday())
+print(a)
+
+#34、数据库优化查询方法
+#外键、索引、联合查询、选择特定字段等等
+
+#35、请列出你会的任意一种统计图（条形图、折线图等）绘制的开源库，第三方也行
+# pychart,matplotlib,seaborn
+
+#36、写一段自定义异常代码
+def fn():
+    try:
+        for i in range(5):
+            if i > 2:
+                raise Exception("numebr greater than 2")
+    except Exception as ret:
+        print(ret)
+fn()
+
+#37、正则表达式匹配中，（.*）和（.*?）匹配区别？
+"""
+（.*）是贪婪匹配，会把满足正则的尽可能多的往后匹配
+（.*?）是非贪婪匹配，会把满足正则的尽可能少匹配
+"""
+
+#38、简述Django的orm
+"""
+ORM，全拼Object-Relation Mapping，意为对象-关系映射
+实现了数据模型与数据库的解耦，通过简单的配置就可以轻松更换数据库，
+而不需要修改代码只需要面向对象编程,
+orm操作本质上会根据对接的数据库引擎，翻译成对应的sql语句,
+所有使用Django开发的项目无需关心程序底层使用的是MySQL、Oracle、sqlite....，
+如果数据库迁移，只需要更换Django的数据库引擎即可
+"""
+
+#39、[[1,2],[3,4],[5,6]]一行代码展开该列表，得出[1,2,3,4,5,6]
+a = [[1,2],[3,4],[5,6]]
+x = [j for i in a for j in i]
+print(x)
+
+import numpy as np
+b = np.array(a).flatten().tolist()
+print(b)
+#40、x="abc",y="def",z=["d","e","f"],分别求出x.join(y)和x.join(z)返回的结果
+x="abc"
+y="def"
+z=["d","e","f"]
+m=x.join(y)
+n=x.join(z)
+print(m,n)
 
 
 
