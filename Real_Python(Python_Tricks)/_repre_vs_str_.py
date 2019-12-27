6216da05629c6195cf2c4a7bb13ba522ed89f634
jfang <jfang@ong-ong.com>
2019-12-27 18:20:49 +0800

    Update

---
+++ Real_Python(Python_Tricks)/_repre_vs_str_.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 15:12:41 2019
+
+@author: jfang
+"""
+
+import datetime 
+
+today = datetime.date.today()
+print(str(today))
+print(repr(today))
+print(today)
---
+++ Real_Python(Python_Tricks)/itertools.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 11:00:18 2019
+
+@author: jfang
+"""
+
+import itertools
+for p in itertools.permutations("ABCD"):
+    print(p)
